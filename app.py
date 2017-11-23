f8a81fc4f39fca299604b078b8dabf43f823de4d
oier <oier.etxaniz@gmail.com>
2017-11-23 18:36:45 +0100

    changes...

--- app.py
+++ app.py
@@ -45,6 +45,7 @@ if __name__ == '__main__':
     app.run(
        # debug=True,
         host = "0.0.0.0",
-        port = 5000
+        port = 5000,
+        threaded=True,
     )
 # socket.run(app)
