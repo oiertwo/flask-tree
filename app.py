a9a7fb1b2c937e9cec563cdbb8204b91d4dd7176
oier <oier.etxaniz@gmail.com>
2017-11-24 11:47:33 +0100

    stupid me

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import send_from_directory, request
 import tree
 from flask import g
 
-app = Flask(__name__, template_folder="templates")
+app = Flask(__name__, static_folder="static", template_folder="templates")
 app.config['DEBUG'] = True
 
 
